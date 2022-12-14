1a2e0439d97538c88bd9e20b2e44243edf96c7f0
SarangKumar <sarangkumar1578@gmail.com>
2022-12-14 17:18:02 +0530

    Search bar

--- src/App.js
+++ src/App.js
@@ -8,16 +8,17 @@ import Navigation from './components/Navigation';
 //pages
 import About from './pages/About';
 import Homepage from './pages/Homepage';
-import NotFound from './pages/NotFound'
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <div className="App">
-    <Navigation />
+      <Navigation />
       <Routes>
         <Route path='/' element={<Homepage />} />
-        <Route path='/:user' element={<Homepage />} />
         <Route path='/about' element={<About />} />
+        <Route path='/login' element={<Login />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </div>
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -5,11 +5,21 @@ import search from '../resources/images/search-90.png'
 const Homepage = () => {
   const {user} = useParams();
   console.log(user);
+
+  const searchToggle = () =>{
+    let searchBox = document.getElementById('home_search_input');
+    if(searchBox.style.display==='none'){
+      searchBox.style.display='block';
+    }else{
+      searchBox.style.display='none';
+    }
+  }
+
   return (
-    <div className='home_comp'>Homepage {user}
-    <div className='home_search'>
-      <img src={search} className='home_search' alt='search'></img>
-      <input type='text' />
+    <div className='home_comp'>
+    <div className='home_search_div'>
+      <img src={search} className='home_search' alt='search' onClick={searchToggle}/>
+      <input id='home_search_input' placeholder='Search' type='text' />
     </div>
 
     </div>
